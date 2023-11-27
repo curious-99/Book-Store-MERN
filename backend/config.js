1a8191e64e8a84cfa52be5f3529aa8c99a221428
curious-99 <mohit39.ugcs20@iiitranchi.com>
2023-11-27 12:31:24 +0530

    p

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,6 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
- 'mongodb+srv://yadavmohiit2002:root@booksstoremern.vujesjk.mongodb.net/books-collection?retryWrites=true&w=majority'
+//  'mongodb+srv://yadavmohiit2002:root@booksstoremern.vujesjk.mongodb.net/books-collection?retryWrites=true&w=majority'
+
+ "mongodb+srv://yadavmohiit2002:root@booksstoremern.vujesjk.mongodb.net/?retryWrites=true&w=majority"
